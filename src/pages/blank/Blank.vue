deb3d3d863ab4a65e8516e309f9fa5c9209ed198
Nomad <188120530@qq.com>
2021-09-01 23:40:07 +0800

    micro

--- src/pages/blank/Blank.vue
+++ src/pages/blank/Blank.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    <svg-icon name="password" color="pink" :size="40"></svg-icon>
-  </div>
+  <div id="container">4554</div>
 </template>
 
 <script>
+import { loadMicroApp, registerMicroApps, start } from 'qiankun'
 export default {
   data() {
     return {}
+  },
+  mounted() {
+    this.loadSubApp()
+    // this.loadSubApps()
+  },
+  methods: {
+    loadSubApp() {
+      loadMicroApp({
+        name: 'reactApp',
+        entry: '//localhost:8090',
+        container: '#container'
+      })
+    },
+    loadSubApps() {
+      registerMicroApps([
+        {
+          name: 'reactApp',
+          entry: '//localhost:8090',
+          container: '#container',
+          activeRule: '/#/micro-app'
+        }
+      ])
+      start()
+    }
   }
 }
 </script>
